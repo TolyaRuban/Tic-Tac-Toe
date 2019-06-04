872dfb242a1a217277f1aacababf1ca4bdfb5323
TolyaRuban <tolyaruban@gmail.com>
2019-06-04 18:55:59 +0300

    Game done

---
+++ src/Board.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Cell from "./Cell";
+
+class Board extends React.Component {
+  renderCells() {
+    const { cells, onCellClicked } = this.props;
+    return cells.map((cell, index) => {
+      return (
+        <Cell
+          value={cell}
+          key={"cell-" + index}
+          onDivClicked={() => {
+            onCellClicked(index);
+          }}
+        />
+      );
+    });
+  }
+
+  render() {
+    return <div className="board">{this.renderCells()}</div>;
+  }
+}
+
+export default Board;
